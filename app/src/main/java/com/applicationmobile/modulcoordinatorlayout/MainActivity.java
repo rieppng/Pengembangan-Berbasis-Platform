43d06645b6183d7ddf193e84d6a459ae76d3d066
Afdhal Fidi Ansori <afdhalfidi.ansori015@gmail.com>
2026-04-28 21:23:36 +0700

    Modul Coordinator Layout

---
+++ app/src/main/java/com/applicationmobile/modulcoordinatorlayout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.applicationmobile.modulcoordinatorlayout;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
